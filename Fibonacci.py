93f446f9470f63aba1844010ab21e47124794602
VivianPatrick <36530732+VivianPatrick@users.noreply.github.com>
2020-05-07 22:31:54 +0100

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,1 +1,16 @@
+#function to compute fibonacci, 
+#f(n) = f(n - 1) + f(n - 2)
 
+def fibonacci(nth):
+    series = []
+    i = 0
+    
+    while len(series) <= nth:
+        if i == 0:
+            series.append(0)
+        elif i == 1:
+            series.append(1)
+        else: 
+            series.append(series[i - 1] + series[i - 2])
+        i += 1
+    return series
